db890a23aba9187f1196c80803a60d396d4a5d04
feffe <feffe@feffe.dev>
2019-12-11 14:45:58 +0100

    run processes

--- Lib.hs
+++ Lib.hs
@@ -6,24 +6,29 @@ import GRM.Abs
 import GRM.Par (pCmd, myLexer)
 import GRM.ErrM
 import System.Process
+import Control.Exception
 
 someFunc :: IO ()
 someFunc = do
     cmdstr <- getLine
-    let tree = case pCmd (myLexer cmdstr) of
-            Ok t -> t
-            Bad err -> error err
-    interpret tree
-    return ()
+    case pCmd (myLexer cmdstr) of
+            Ok t ->  interpret t
+            Bad err -> putStrLn err
+    someFunc
 
 
 interpret :: Cmd -> IO ()
 interpret (Cmd []) = return ()
 interpret (Cmd prgs) = mapM_ progrun prgs
 
+try' :: IO a ->  IO (Either IOException a)
+try' =  try 
 
 progrun :: Prg -> IO ()
 progrun (NPrg (Ident p) args) = do 
-    proc <- createProcess (proc p [])
+    res <- try' $ (createProcess (proc p []))
+    case res of 
+        Left ex -> putStrLn $ show ex
+        Right _ -> return ()
     return ()
 
