eed663c0e22862b58f851a4afe4a5f5e224a343b
feffe <feffe@feffe.dev>
2019-12-18 21:42:12 +0100

    experimental prog

--- Lib.hs
+++ Lib.hs
@@ -16,11 +16,12 @@ import System.Posix.Signals
 import Parse (parse, Command(..), Program(..))
 import System.Console.Haskeline
 import Control.Concurrent
-
+import System.Process.Internals (GroupID(..))
 
 setup :: IO ()
-setup = hSetBuffering stdout NoBuffering
-  --void $ installHandler sigINT Ignore Nothing
+setup = do
+  hSetBuffering stdout NoBuffering
+  void $ installHandler sigINT Ignore Nothing
 
 
 
@@ -52,40 +53,44 @@ try' =  try
 
 
 runProgsPipe :: Command -> IO ()
-runProgsPipe (Foreground prgs@(Prog exec args : rest)) = foregroundPipe prgs (UseHandle stdin)
+runProgsPipe (Foreground prgs@(Prog exec args : rest)) = foregroundPipe prgs 0 (UseHandle stdin)
 runProgsPipe (Background prgs) = do
-  forkIO $ foregroundPipe prgs CreatePipe
+  forkIO $ foregroundPipe prgs 0 CreatePipe
   return ()
 
 
-foregroundPipe :: [Program] -> StdStream -> IO ()
-foregroundPipe [Prog "cd" args] inpipe = do 
+foregroundPipe :: [Program] -> GroupID -> StdStream -> IO ()
+foregroundPipe [Prog "cd" args] _ inpipe = do 
   homedir <- getHomeDirectory
   case args of
     [] -> setCurrentDirectory homedir
     [x] -> setCurrentDirectory x
     (x:xs) -> putStrLn "cd: only takes one argument"
-foregroundPipe [Prog exec args] inpipe = do
-  let process = (proc exec args){std_in = inpipe}
+foregroundPipe [Prog exec args] grpid inpipe = do
+  let process = case grpid of 
+                  0 -> (proc exec args){std_in = inpipe, create_group = True }
+                  i -> (proc exec args){std_in = inpipe, child_group = Just i }
   res <- try' $ createProcess process
   case res of
     Left ex -> print ex
     Right (_,_,_,phandle) -> void $ waitForProcess phandle
   print "Done"
   return ()
-foregroundPipe (Prog "cd" args : rest) inpipe = do 
+foregroundPipe (Prog "cd" args : rest) grpid inpipe = do 
   homedir <- getHomeDirectory
   case args of
     [] -> setCurrentDirectory homedir
     [x] -> setCurrentDirectory x
     (x:xs) -> putStrLn "cd: only takes one argument"
-  foregroundPipe rest CreatePipe
-foregroundPipe (Prog exec args : rest) inpipe = do
-  let process = (proc exec args){std_in = inpipe, std_out = CreatePipe}
+  foregroundPipe rest grpid CreatePipe
+foregroundPipe (Prog exec args : rest) grpid inpipe = do
+  let process = case grpid of 
+                  0 -> (proc exec args){std_in = inpipe, create_group = True }
+                  i -> (proc exec args){std_in = inpipe, child_group = Just i }  
   res <- try' $ createProcess process
   case res of
     Left ex -> print ex
     Right (_,Just so,_,phandle) -> do
-      foregroundPipe rest (UseHandle so)
+      foregroundPipe rest (getGid)(UseHandle so)
       void $ waitForProcess phandle
   return ()
